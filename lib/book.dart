8d8f3f6e133c6ecd009073c2c19e0cb3a1da3c80
zhengw <247276359@qq.com>
2019-07-11 15:55:59 +0800

    文件读写 研究2

--- lib/book.dart
+++ lib/book.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
-import 'package:path/path.dart' as path;
-import 'package:epub/epub.dart';
+
+import 'package:epub/epub.dart' as equb;
 import 'package:path_provider/path_provider.dart';
 import 'package:dio/dio.dart';
 
-import 'dart:convert';
 import 'dart:async';
+import 'package:flutter_html/flutter_html.dart';
 
 class Book extends StatefulWidget {
   @override
@@ -17,7 +17,7 @@ class _BookState extends State<Book> {
   Future<File> _getLocalFile() async {
     // get the path to the document directory.
     String dir = (await getApplicationDocumentsDirectory()).path;
-    return new File('$dir/counter.txt');
+    return new File('$dir/counter1.txt');
   }
 
   Future<int> _readCounter() async {
@@ -34,6 +34,9 @@ class _BookState extends State<Book> {
   int _counter;
   String title;
   List chapters = [];
+  equb.EpubChapter content;
+  var coverImage;
+  List books;
 
   @override
   void initState() {
@@ -44,6 +47,25 @@ class _BookState extends State<Book> {
         _counter = value;
       });
     });
+    _getBooks();
+  }
+
+  _getBooks() async {
+    try {
+      Directory directory = await getApplicationDocumentsDirectory();
+      List booksArr = [];
+      directory.listSync().forEach((list) {
+        String name = list.path.substring(list.path.lastIndexOf('/') + 1);
+        if (name.indexOf('.') > -1) {
+          booksArr.add({'name': name.substring(0, name.indexOf('.')), 'path': list.path});
+        }
+      });
+      setState(() {
+        books = booksArr;
+      });
+    } on FileSystemException {
+      return 0;
+    }
   }
 
   Future<Null> _incrementCounter() async {
@@ -57,7 +79,7 @@ class _BookState extends State<Book> {
   Future download2() async {
     try {
       Response response = await Dio().get(
-        'http://d18.aixdzs.com/184/184207/184207.epub',
+        'http://2732256.174.ctc.data.tv002.com/down/19a5cc972c7a19ee03c918e270b3912c/San%20Guo%20Yan%20Yi%20_Xiao%20Zhu%20Ben%20%28S%20-%20%28Ming%20%29Luo%20Guan%20Zhong.epub?cts=dx-f-D27A151A97A54Fe5fcd&ctp=27A151A97A54&ctt=1562803450&limit=1&spd=30000&ctk=19a5cc972c7a19ee03c918e270b3912c&chk=4dc5c27d098bb794d2927cde0a6f5db8-1598714',
         onReceiveProgress: showDownloadProgress,
         //Received data with List<int>
         options: Options(
@@ -67,7 +89,7 @@ class _BookState extends State<Book> {
       );
       print(response.headers);
       String dir = (await getApplicationDocumentsDirectory()).path;
-      File file = new File(dir + '/sanguoyanyi.epub');
+      File file = new File(dir + '/三国演义.epub');
       var raf = file.openSync(mode: FileMode.write);
       // response.data is List<int> type
       raf.writeFromSync(response.data);
@@ -79,15 +101,17 @@ class _BookState extends State<Book> {
 
   Future<int> read2() async {
     try {
-      String dir = (await getApplicationDocumentsDirectory()).path;
-      File file = new File(dir + '/sanguoyanyi.epub');
+      Directory directory = await getApplicationDocumentsDirectory();
+//      list
+      String dir = directory.path;
+//      print(directory.listSync().length);
+      File file = new File(dir + '/三国演义.epub');
       // read the variable as a string from the file.
 //      String contents = await file.readAsString();
       List<int> bytes = await file.readAsBytes();
 
 // Opens a book and reads all of its content into memory
-      EpubBook epubBook = await EpubReader.readBook(bytes);
-      print(epubBook.Title);
+      equb.EpubBook epubBook = await equb.EpubReader.readBook(bytes);
 
 //      epubBook.Chapters.forEach((EpubChapter chapter) {
 //        // Title of chapter
@@ -104,11 +128,68 @@ class _BookState extends State<Book> {
       setState(() {
         title = epubBook.Title;
         chapters = epubBook.Chapters;
+        coverImage = epubBook.CoverImage;
       });
 //      return int.parse(contents);
     } on FileSystemException {
       return 0;
     }
+  }
+
+  _delBook(path) async {
+    try {
+      Directory directory = await getApplicationDocumentsDirectory();
+      directory.listSync().forEach((list) {
+//        print(list.path.substring(list.path.lastIndexOf('/') + 1));
+        if (path == list.path) {
+          list.delete();
+        }
+      });
+      _getBooks();
+    } on FileSystemException {
+      return 0;
+    }
+  }
+
+  _deleteBook(context, path, name) async {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('删除提示'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('确认删除 $name'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('取消'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('确定'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _delBook(path);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _readIndex(index) {
+    Navigator.of(context).pop();
+    setState(() {
+      content = chapters[index];
+    });
   }
 
   void showDownloadProgress(received, total) {
@@ -119,9 +200,40 @@ class _BookState extends State<Book> {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('文件读写'),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+                child: Container(
+              child: coverImage != null
+                  ? Container(
+                      width: 100,
+                      height: 100,
+                    )
+                  : Placeholder(
+                      fallbackWidth: 1,
+                      fallbackHeight: 1,
+                    ),
+            )),
+            Column(
+              children: chapters.map<Widget>((item) {
+                return InkWell(
+                  onTap: () {
+                    _readIndex(chapters.indexOf(item));
+                  },
+                  child: Container(
+                    child: Text('${item.Title}'),
+                  ),
+                );
+              }).toList(),
+            )
+          ],
+        ),
       ),
       body: Container(
         child: ListView(
@@ -154,12 +266,20 @@ class _BookState extends State<Book> {
                         children: chapters.map<Widget>((item) {
                         return Column(
                           children: <Widget>[
-                            Container(
-                              child: Text('${item.Title}'),
-                            ),
-                            Container(
-                              child: Text('${item.HtmlContent}'),
-                            )
+//                          Container(
+//                            child: Text('${item.Title}'),
+//                          ),
+//                            Container(
+//                            child: Text('${item.HtmlContent}'),
+//                          ),
+//                            Container(
+//                              child: Html(
+//                                  data: '${item.HtmlContent}',
+////                                padding: EdgeInsets.all(2.0),
+//                                  linkStyle: const TextStyle(
+//                                    color: Colors.redAccent,
+//                                  )),
+//                            )
                           ],
                         );
                       }).toList())
@@ -167,8 +287,65 @@ class _BookState extends State<Book> {
                         fallbackWidth: 1,
                         fallbackHeight: 1,
                       ),
+                content != null
+                    ? Container(
+                        child: Html(
+                          data: '${content.HtmlContent}',
+                          onLinkTap: (url) {
+                            print("Opening $url...");
+                          },
+//                    customTextAlign: (dom.Node node) {
+//                      if (node is dom.Element) {
+//                        switch (node.localName) {
+//                          case "p":
+//                            return TextAlign.justify;
+//                        }
+//                      }
+//                    },
+                        ),
+                      )
+                    : Placeholder(
+                        fallbackWidth: 1,
+                        fallbackHeight: 1,
+                      )
               ],
-            )
+            ),
+            books != null
+                ? Container(
+                    padding: EdgeInsets.only(left: 5, right: 5, top: 10),
+                    child: Wrap(
+                      runSpacing: 10,
+                      alignment: WrapAlignment.start,
+                      children: books.map<Widget>((item) {
+                        return Container(
+                          width: (width - 10) / 4,
+                          height: (width - 10) / 4 * 3 / 2,
+                          padding: EdgeInsets.only(left: 5, right: 5),
+                          child: Container(
+                            decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+                            child: Stack(
+                              fit: StackFit.expand,
+                              children: <Widget>[
+                                Center(
+                                  child: Text(item['name']),
+                                ),
+                                Positioned(
+                                    bottom: 0,
+                                    right: 0,
+                                    child: IconButton(
+                                        color: Colors.red,
+                                        icon: Icon(Icons.delete),
+                                        onPressed: () {
+                                          _deleteBook(context, item['path'], item['name']);
+                                        }))
+                              ],
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  )
+                : Container(),
           ],
         ),
       ),
